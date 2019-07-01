43aedf692080b06d32d13662665969f79a9485e7
Administrator <zxzx>
2019-07-01 17:27:57 +0800

    Initial commit

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <!--<canvas id="canvas" width="500" height="400" style="position:absolute;top:0;left:0;">
-      您的浏览器不支持canvas标签。
-    </canvas>-->
   </div>
 </template>
 
@@ -12,8 +9,6 @@
 export default {
   name: 'App',
   mounted() {
-    //const ss = require("./assets/js/test");
-    //ss();
   }
 }
 </script>
