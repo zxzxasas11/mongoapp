95038fb0dadf70d6ee2c47184f360f0101963a25
Administrator <zxzx>
2019-08-13 14:44:01 +0800

    hero

--- web/src/api/fgo/hero.js
+++ web/src/api/fgo/hero.js
@@ -3,6 +3,6 @@ import fetch from '../fetch';
 export default {
     //查询所有
     getAll(params) {
-        return fetch.get('/article/getAll', params);
+        return fetch.get('/hero/getAll', params);
     },
 }
