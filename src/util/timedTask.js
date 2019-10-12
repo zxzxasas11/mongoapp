154ea9449895670257d7c436188cc2d03235e304
Administrator <zxzx>
2019-10-12 17:38:42 +0800

    1009

--- src/util/timedTask.js
+++ src/util/timedTask.js
@@ -1,8 +1,12 @@
 const schedule = require('node-schedule');
 const JDLYevery = require('../spider/JDLYevery');
-
+const MEZIevery = require('../spider/fixMeizitu');
 module.exports=async function(){
     schedule.scheduleJob('00 00 12 * * *',async ()=>{
         await JDLYevery();
+    });
+
+    schedule.scheduleJob('00 00 13 * * *',async ()=>{
+        await MEZIevery();
     });
 };
